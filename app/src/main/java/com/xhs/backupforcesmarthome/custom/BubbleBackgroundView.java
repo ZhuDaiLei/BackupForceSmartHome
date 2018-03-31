84b8dc2c930e707b8e93985e8a47e4c1b70e23ef
ZengXiao <1058083107@qq.com>
2018-03-31 14:51:07 +0800

    修改自动泡泡机线程控制系统，原来用Rx，现在用线程池

--- app/src/main/java/com/xhs/backupforcesmarthome/custom/BubbleBackgroundView.java
+++ app/src/main/java/com/xhs/backupforcesmarthome/custom/BubbleBackgroundView.java
@@ -1,28 +1,27 @@
 package com.xhs.backupforcesmarthome.custom;
 
-import android.animation.Animator;
 import android.animation.ObjectAnimator;
-import android.app.Activity;
 import android.content.Context;
-import android.graphics.Rect;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 import android.widget.ImageView;
 
 import com.xhs.backupforcesmarthome.R;
-import com.xhs.backupforcesmarthome.utils.MyPhoneUtil;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
-import io.reactivex.internal.operators.observable.ObservableTimer;
 import io.reactivex.schedulers.Schedulers;
 
 
@@ -33,55 +32,97 @@ import io.reactivex.schedulers.Schedulers;
  * description
  */
 public class BubbleBackgroundView extends ViewGroup {
-    private boolean isFirst = true;
     private Context context;
-    private boolean running = true;
-    private Rect rect;
+    private Bitmap bitmap;
+    private ScheduledExecutorService scheduledThreadPool;
+    private Thread myThread;
+    private boolean flag = false;
 
     public BubbleBackgroundView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
-        rect = new Rect(0, 0, MyPhoneUtil.getScreenWidth((Activity) context), MyPhoneUtil.getScreenHeight((Activity) context));
+        //初始化泡泡
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bubble);
+        //初始化线程池
+        scheduledThreadPool = Executors.newScheduledThreadPool(1);
+        //初始化线程
+        myThread = new MyThread();
     }
 
+    /**
+     * 设置父控件宽高
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
+    /**
+     * 设置子控件位置
+     * @param b
+     * @param i
+     * @param i1
+     * @param i2
+     * @param i3
+     */
     @Override
     protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
-        Consumer consumer = new Consumer<Long>() {
-            @Override
-            public void accept(Long aLong) throws Exception {
-                if (getChildCount() >= 10) {
+        scheduledThreadPool.schedule(myThread, 2000, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 线程回调到主线程
+     */
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1) {
+                if (getChildCount() >= 20) {
                     removeView(getChildAt(0));
                 }
-                if (getChildCount() < 10) {
+                if (getChildCount() < 20) {
                     ImageView imageView = new ImageView(context);
-                    imageView.setImageResource(R.drawable.bubble);
+                    imageView.setImageBitmap(bitmap);
                     imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                     addView(imageView);
                     setChildLayout(imageView);
-                    //setAnimation(imageView);
                 }
             }
-        };
-        Observable timer = Observable.timer(1, TimeUnit.SECONDS);
+        }
+    };
 
-        timer.observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(consumer);
 
+    /**
+     * 子线程，在线程中进行泡泡生成与添加
+     */
+    private class MyThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            handler.sendEmptyMessage(1);
+        }
     }
 
+    /**
+     * 设置子控件的位置
+     * @param imageView 子控件
+     */
     private void setChildLayout(ImageView imageView) {
-        int left = new Random().nextInt(getWidth());
-        int top = new Random().nextInt(getHeight());
-        int test = new Random().nextInt(4);
-        long a = new Random().nextInt(4000) + 10000;
-        long b1 = new Random().nextInt(4000) + 10000;
+        //随机距离左面的距离，控制从控件上面出来还是下面出来
+        int left = new Random().nextInt(getWidth() - bitmap.getWidth()) + bitmap.getWidth();
+        //随机距离顶部的距离，控制从左面和右面出来
+        int top = new Random().nextInt(getHeight() - bitmap.getHeight()) + bitmap.getHeight();
+        //随机四个方位
+        int direction = new Random().nextInt(4);
+        //随机泡泡x、y轴的运动时间
+        long a = new Random().nextInt(8000) + 15000;
+        long b1 = new Random().nextInt(8000) + 15000;
+        //随机参数控制是往左飘还是往右飘
         int c = new Random().nextInt(2);
+        //随机参数控制是往上飘还是往下飘
         int f = new Random().nextInt(2);
         int d = 1;
         int e = 1;
@@ -91,60 +132,41 @@ public class BubbleBackgroundView extends ViewGroup {
         if (f == 0) {
             e = -1;
         }
-        switch (test) {
+        //方位设置
+        switch (direction) {
             case 0:
-                imageView.layout(0, 0, left, 50);
-                Log.e("创建成功", "------------0");
+                //从上面出来的泡泡
+                imageView.layout(left, 0, left + bitmap.getWidth(), bitmap.getHeight());
+                //控制在y轴上只能往下飘
                 e = 1;
                 break;
             case 1:
-                imageView.layout(0, 0, 50, top);
-                Log.e("创建成功", "------------1");
-                d = 1;
-                break;
-            case 2:
-                imageView.layout(getWidth() - left, getHeight() - 50, getWidth(), getHeight());
-                Log.e("创建成功", "------------2");
+                //从下面出来的泡泡
+                imageView.layout(left, getHeight() - bitmap.getHeight(), left + bitmap.getWidth(), getHeight());
+                //控制在y轴上只能往上飘
                 e = -1;
                 break;
+            case 2:
+                //从左面出来的泡泡
+                imageView.layout(0, top, bitmap.getWidth(), bitmap.getHeight() + top);
+                //控制在x轴上只能往右飘
+                d = 1;
+                break;
             case 3:
-                imageView.layout(getWidth() - 50, getHeight() - top, getWidth(), getHeight());
-                Log.e("创建成功", "------------3");
+                //从右面出来的泡泡
+                imageView.layout(getWidth() - bitmap.getWidth(), top, getWidth(), bitmap.getHeight() + top);
+                //控制在x轴上只能往左飘
                 d = -1;
                 break;
             default:
         }
-        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "translationX", 0, d * (getWidth() + 100));
+        //设置动画
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "translationX", 0, d * (getWidth() + bitmap.getWidth()));
         animator.setDuration(a);
-        ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageView, "translationY", 0, e * (getHeight() + 100));
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageView, "translationY", 0, e * (getHeight() + bitmap.getHeight()));
         animator1.setDuration(b1);
         animator.start();
         animator1.start();
     }
 
-
-    private void setAnimation(ImageView imageView) {
-        long a = new Random().nextInt(4000) + 9000;
-        long b1 = new Random().nextInt(4000) + 9000;
-        int c = new Random().nextInt(2);
-        int f = new Random().nextInt(2);
-        int d = 1;
-        int e = 1;
-        if (c == 0) {
-            d = -1;
-        }
-        if (f == 0) {
-            e = -1;
-        }
-        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "translationX", 0, d * (getWidth() + 100));
-        animator.setDuration(a);
-        ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageView, "translationY", 0, e * (getHeight() + 100));
-        animator1.setDuration(b1);
-        animator.start();
-        animator1.start();
-    }
-
-    public void stopRunning() {
-        running = false;
-    }
 }
